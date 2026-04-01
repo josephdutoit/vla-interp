d27822791ea3e309a25e4eb3da3c17258e4f265c
joseph.dutoit <josephdutoit5@gmail.com>
2026-04-01 12:15:02 -0600

    causal tracing finished

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 1. Load System Modules
+module load ffmpeg
+module load cuda/12.1
+
+# 2. Activate Python Environment
+source /home/jcdutoit/CS601/vla-interp/.venv/bin/activate
+
+# 3. Fix the "Rats" (BYU-Specific Pathing)
+export FFMPEG_LIB=/apps/spack/root/opt/spack/linux-rhel9-haswell/gcc-13.2.0/ffmpeg-7.0.1-pzg5pllmqfjzz2ubrlm3jcxyyh7gtpyr/lib
+export LD_LIBRARY_PATH=$FFMPEG_LIB:$LD_LIBRARY_PATH
+
+# 4. FIPS Security Bypass
+export LD_PRELOAD=/usr/lib64/libcrypto.so.1.1.1k:/usr/lib64/libssl.so.1.1.1k
+
+# 5. Headless Rendering for Simulation
+export MUJOCO_GL=egl
+export PYOPENGL_PLATFORM=egl
+# export HF_HUB_OFFLINE=1
+
+echo "VLA-0 Environment Ready."
